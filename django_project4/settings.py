f2004883ddb8624c69a95463312d2c4fa7e64fdc
Sarah-LB <slloving@gmail.com>
2022-11-27 15:41:00 +0000

    add space to procfile

--- django_project4/settings.py
+++ django_project4/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 import dj_database_url
+
 from pathlib import Path
 if os.path.exists("env.py"):
     import env
@@ -199,5 +200,3 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'homelearning@example.com'
-
-ACCOUNT_EMAIL_VERIFICATION = 'none
