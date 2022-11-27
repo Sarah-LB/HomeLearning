c0c9f2280b7b6dc7e8d0ecd0457e454d1064410a
Sarah-LB <slloving@gmail.com>
2022-11-27 14:43:06 +0000

    fix deploy issues

--- django_project4/settings.py
+++ django_project4/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['milestone-p4.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['https://git.heroku.com/milestone-p4.git', 'localhost']
 
 
 # Application definition
