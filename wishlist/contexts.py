428daa0f3989f1ba4380a8cc622b9353f82da8e4
Sarah-LB <slloving@gmail.com>
2022-12-05 20:44:55 +0000

    update wishlist template and context processor!

--- wishlist/contexts.py
+++ wishlist/contexts.py
@@ -10,6 +10,16 @@ def wishlist_contents(request):
     product_count = 0
     wishlist = request.session.get('wishlist', {})
 
+    for item_id, quantity in wishlist.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        wishlist_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
     context = {
         'wishlist_items': wishlist_items,
         'product_count': product_count,
