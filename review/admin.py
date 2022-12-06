4a6196d55d9cf1ea58d44e2be6ebdbe5ca17c79a
Sarah-LB <slloving@gmail.com>
2022-12-06 00:42:41 +0000

    add UserReview admin

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import UserReview
 
 # Register your models here.
+
+
+class (admin.ModelAdmin):
+
+    readonly_fields = ('user', 'product',
+                       'date', 'updated',
+                       'title', 'content')
+
+    fields = ('user', 'product',
+                'date', 'updated',
+                'title', 'content')
+
+    list_display = ('user', 'product',
+                       'date', 'updated',
+                       'title')
+
+    ordering = ('-date', 'product', 'user')
+
+
+admin.site.register(UserReview, UserReviewAdmin)
